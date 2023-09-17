5777a4e0be2dc9c6ef05f593ebfd4fccb27b29de
Allen Pozo <pm201803@upes.edu.sv>
2023-09-16 21:55:53 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/SecretController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSecurityDemo.Controllers
+{
+    [Authorize(Users ="grace.hernandez@upes.edu.sv")]
+    public class SecretController : Controller
+    {
+        // GET: Secret
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      // [Authorize]
+        public ContentResult Secret()
+        {
+            return Content("Secret information");
+        }
+
+        [AllowAnonymous]
+        public ContentResult PublicInfo()
+        {
+            return Content("Secret information here");
+        }
+
+    }
+}
